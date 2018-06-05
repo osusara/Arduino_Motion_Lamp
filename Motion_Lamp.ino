c9b53e9cf8ca983936d81d31d7a756ace8a929d4
Osusara Kammalawatta <30489601+osusara@users.noreply.github.com>
2018-06-05 16:54:55 +0530

    Added .ino file

---
+++ Motion_Lamp.ino
@@ -0,0 +1,110 @@
+int echo = 4;
+int trig = 3;
+long tim;
+long dis;
+void setup() {
+
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(10,OUTPUT);
+  pinMode(11,OUTPUT);
+  pinMode(12,OUTPUT);
+  pinMode(13,OUTPUT);
+  
+  pinMode(echo,INPUT); //Ultra Sonic
+  pinMode(trig,OUTPUT);
+  Serial.begin(9600);
+
+  pinMode(2,INPUT); //IR
+}
+
+void loop() {
+  digitalWrite(trig,LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(trig,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig,LOW);
+
+  tim = pulseIn(echo,HIGH);
+  dis = tim*0.034/2;
+
+  if(dis < 10){
+    digitalWrite(6,HIGH);
+    digitalWrite(7,HIGH);
+    digitalWrite(8,HIGH);
+    digitalWrite(9,HIGH);
+    digitalWrite(10,HIGH);
+    digitalWrite(11,HIGH);
+    digitalWrite(12,HIGH);
+    digitalWrite(13,HIGH);
+  }
+
+  else if(dis < 14){
+    digitalWrite(6,LOW);
+    digitalWrite(7,LOW);
+    digitalWrite(8,HIGH);
+    digitalWrite(9,HIGH);
+    digitalWrite(10,HIGH);
+    digitalWrite(11,HIGH);
+    digitalWrite(12,HIGH);
+    digitalWrite(13,HIGH);
+  }
+
+  else if(dis < 18){
+    digitalWrite(6,LOW);
+    digitalWrite(7,LOW);
+    digitalWrite(8,LOW);
+    digitalWrite(9,LOW);
+    digitalWrite(10,HIGH);
+    digitalWrite(11,HIGH);
+    digitalWrite(12,HIGH);
+    digitalWrite(13,HIGH);
+  }
+  
+  else if(dis < 22){
+    digitalWrite(6,LOW);
+    digitalWrite(7,LOW);
+    digitalWrite(8,LOW);
+    digitalWrite(9,LOW);
+    digitalWrite(10,LOW);
+    digitalWrite(11,LOW);
+    digitalWrite(12,HIGH);
+    digitalWrite(13,HIGH);
+
+  }
+
+  else if(dis < 30){
+    digitalWrite(6,LOW);
+    digitalWrite(7,LOW);
+    digitalWrite(8,LOW);
+    digitalWrite(9,LOW);
+    digitalWrite(10,LOW);
+    digitalWrite(11,LOW);
+    digitalWrite(12,LOW);
+    digitalWrite(13,LOW);
+
+}
+  while(digitalRead(2)==LOW){
+    digitalWrite(6,HIGH);
+    digitalWrite(7,HIGH);
+    digitalWrite(8,HIGH);
+    digitalWrite(9,HIGH);
+    digitalWrite(10,HIGH);
+    digitalWrite(11,HIGH);
+    digitalWrite(12,HIGH);
+    digitalWrite(13,HIGH);
+    delay(500);
+    digitalWrite(6,LOW);
+    digitalWrite(7,LOW);
+    digitalWrite(8,LOW);
+    digitalWrite(9,LOW);
+    digitalWrite(10,LOW);
+    digitalWrite(11,LOW);
+    digitalWrite(12,LOW);
+    digitalWrite(13,LOW);
+    delay(500);
+  }
+}
